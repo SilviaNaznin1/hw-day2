54ae4859f006aac36ac5ec1c73666ee30df84115
SilviaNaznin1 <silvianazninetu@gmail.com>
2025-04-26 13:01:11 +0600

    Initial commit

---
+++ L2 HW.R
@@ -0,0 +1,24 @@
+#question 1 Compute the difference between this year (2025) and the year you started at the university and divide this by the difference between this year and the year you were born. Multiply this with 100 to get the percentage of your life you have spent at the university.
+
+current_year = 2025
+admission_year = 2019
+birth_year = 2001
+
+answer1 = (current_year - admission_year)/(current_year - birth_year) * 100
+answer1
+
+
+
+#Make different kinds of variables and vectors with the data types we learned together.
+#variables
+x = 5
+y = 10.0
+z = 13.4
+
+#characters
+vector_number = c(1, 2, 2, 3, 5)
+vector_character = c("xyz", "abc", "mno")
+vector_float = c(2.34, 10, 5.657)
+
+
+
